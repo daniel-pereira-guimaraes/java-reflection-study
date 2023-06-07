6bd0a8eeb6893236faf7d4b262829ac953f66131
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-06-06 20:18:26 -0400

    Created Movie class, inherited from Product class.

---
+++ src/main/java/model/Movie.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Movie extends Product {
+	
+	public static enum Category {
+		ACTION,
+		COMEDY,
+		DRAMA, 
+		FANTASY,
+		HORROR,
+		MYSTERY,
+		ROMANCE	
+	}
+	
+	private static double MIN_MINUTES = 5;
+	public static int HORROR_MINIMUM_AGE = 18;
+	
+	private Category category;
+	private Integer minutes;
+	
+	public Category getCategory() {
+		return category;
+	}
+	
+	public Integer getMinutes() {
+		return minutes;
+	}
+	
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public void setMinutes(Integer minutes) {
+		if (minutes < MIN_MINUTES) {
+			throw new RuntimeException("Invalid minutes: " + minutes);
+		}
+		this.minutes = minutes;
+	}
+	
+}
